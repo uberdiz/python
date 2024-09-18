588799d30d282a6f80e3abf58e9b514f89759ee1
uberdiz <sebastian.r.langdon@gmail.com>
2024-09-18 19:29:48 -0400

    X's and O's project_1.26(Check_Tic_Tac_Toe).py

--- project_1.26(Check_Tic_Tac_Toe).py
+++ project_1.26(Check_Tic_Tac_Toe).py
@@ -1,6 +1,6 @@
-board = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
+board = [[" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]]
 winX = False
 winO = False
 gaming = True
@@ -18,47 +18,47 @@ def print_board():
    print(ho, ve, ho2, ve2, ho3, ve3, ho4)
 def playX():
     X = input("You are X, enter your move: ")
-    if X == "a1" and board[0][0] == 0:
-       board[0][0] = 1
-    elif X == "b1" and board[0][1] == 0:
-       board[0][1] = 1
-    elif X == "c1"and board[0][2] == 0:
-       board[0][2] = 1
-    elif X == "a2" and board[1][0] == 0:
-       board[1][0] = 1
-    elif X == "b2" and board[1][1] == 0:
-       board[1][1] = 1
-    elif X == "c2" and board[1][2] == 0:
-       board[1][2] = 1
-    elif X == "a3" and board[2][0] == 0:
-       board[2][0] = 1
-    elif X == "b3" and board[2][1] == 0:
-       board[2][1] = 1
-    elif X == "c3" and board[2][2] == 0:
-       board[2][2] = 1
+    if X == "a1" and board[0][0] == " ":
+       board[0][0] = "X"
+    elif X == "b1" and board[0][1] == " ":
+       board[0][1] = "X"
+    elif X == "c1"and board[0][2] == " ":
+       board[0][2] = "X"
+    elif X == "a2" and board[1][0] == " ":
+       board[1][0] = "X"
+    elif X == "b2" and board[1][1] == " ":
+       board[1][1] = "X"
+    elif X == "c2" and board[1][2] == " ":
+       board[1][2] = "X"
+    elif X == "a3" and board[2][0] == " ":
+       board[2][0] = "X"
+    elif X == "b3" and board[2][1] == " ":
+       board[2][1] = "X"
+    elif X == "c3" and board[2][2] == " ":
+       board[2][2] = "X"
     else:
         print("Invalid move, try again")
         playX()
 def playO():
     O = input("You are O, enter your move: ")
-    if O == "a1" and board[0][0] == 0:
-        board[0][0] = 2
-    elif O == "b1" and board[0][1] == 0:
-        board[0][1] = 2
-    elif O == "c1" and board[0][2] == 0:
-        board[0][2] = 2
-    elif O == "a2" and board[1][0] == 0:
-        board[1][0] = 2
-    elif O == "b2" and board[1][1] == 0:
-        board[1][1] = 2
-    elif O == "c2" and board[1][2] == 0:
-        board[1][2] = 2
-    elif O == "a3" and board[2][0] == 0:
-        board[2][0] = 2
-    elif O == "b3" and board[2][1] == 0:
-        board[2][1] = 2
-    elif O == "c3" and board[2][2] == 0:
-        board[2][2] = 2
+    if O == "a1" and board[0][0] == " ":
+        board[0][0] = "O"
+    elif O == "b1" and board[0][1] == " ":
+        board[0][1] = "O"
+    elif O == "c1" and board[0][2] == " ":
+        board[0][2] = "O"
+    elif O == "a2" and board[1][0] == " ":
+        board[1][0] = "O"
+    elif O == "b2" and board[1][1] == " ":
+        board[1][1] = "O"
+    elif O == "c2" and board[1][2] == " ":
+        board[1][2] = "O"
+    elif O == "a3" and board[2][0] == " ":
+        board[2][0] = "O"
+    elif O == "b3" and board[2][1] == " ":
+        board[2][1] = "O"
+    elif O == "c3" and board[2][2] == " ":
+        board[2][2] = "O"
     else:
         print("Invalid move, try again")
         playO()
@@ -66,59 +66,59 @@ def win():
    global winX, winO, gaming
    # Win conditions
    # Horizontal X
-   if board[0][0] == board[0][1] == board[0][2] == 1:
+   if board[0][0] == board[0][1] == board[0][2] == "X":
        winX = True
        gaming = False
-   elif board[1][0] == board[1][1] == board[1][2] == 1:
+   elif board[1][0] == board[1][1] == board[1][2] == "X":
        winX = True
        gaming = False
-   elif board[2][0] == board[2][1] == board[2][2] == 1:
+   elif board[2][0] == board[2][1] == board[2][2] == "X":
        winX = True
        gaming = False
    # Vertical X
-   elif board[0][0] == board[1][0] == board[2][0] == 1:
+   elif board[0][0] == board[1][0] == board[2][0] == "X":
        winX = True
        gaming = False
-   elif board[0][1] == board[1][1] == board[2][1] == 1:
+   elif board[0][1] == board[1][1] == board[2][1] == "X":
        winX = True
        gaming = False
-   elif board[0][2] == board[1][2] == board[2][2] == 1:
+   elif board[0][2] == board[1][2] == board[2][2] == "X":
        winX = True
        gaming = False
    # Diagonal X
-   elif board[0][0] == board[1][1] == board[2][2] == 1:
+   elif board[0][0] == board[1][1] == board[2][2] == "X":
        winX = True
        gaming = False
-   elif board[2][0] == board[1][1] == board[0][2] == 1:
+   elif board[2][0] == board[1][1] == board[0][2] == "X":
        winX = True
        gaming = False
 
 
    # Horizontal O
-   elif board[0][0] == board[0][1] == board[0][2] == 2:
+   elif board[0][0] == board[0][1] == board[0][2] == "O":
        winO = True
        gaming = False
-   elif board[1][0] == board[1][1] == board[1][2] == 2:
+   elif board[1][0] == board[1][1] == board[1][2] == "O":
        winO = True
        gaming = False
-   elif board[2][0] == board[2][1] == board[2][2] == 2:
+   elif board[2][0] == board[2][1] == board[2][2] == "O":
        winO = True
        gaming = False
    # Vertical O
-   elif board[0][0] == board[1][0] == board[2][0] == 2:
+   elif board[0][0] == board[1][0] == board[2][0] == "O":
        winO = True
        gaming = False
-   elif board[0][1] == board[1][1] == board[2][1] == 2:
+   elif board[0][1] == board[1][1] == board[2][1] == "O":
        winO = True
        gaming = False
-   elif board[0][2] == board[1][2] == board[2][2] == 2:
+   elif board[0][2] == board[1][2] == board[2][2] == "O":
        winO = True
        gaming = False
    # Diagonal O
-   elif board[0][0] == board[1][1] == board[2][2] == 2:
+   elif board[0][0] == board[1][1] == board[2][2] == "O":
        winO = True
        gaming = False
-   elif board[2][0] == board[1][1] == board[0][2] == 2:
+   elif board[2][0] == board[1][1] == board[0][2] == "O":
        winO = True
        gaming = False
    return winX, winO, gaming
