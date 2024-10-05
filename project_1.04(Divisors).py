d18570f5d7994a9bdccd44e8ba14b3f56ed1fd74
uberdiz <sebastian.r.langdon@gmail.com>
2024-10-05 01:47:38 -0400

    Delete project_1.04(Divisors).py

--- project_1.04(Divisors).py
+++
@@ -1,7 +0,0 @@
-num = int(input("Please give me a number: "))
-divisor_list = list(range(1, num+1))
-divisors = []
-for number in divisor_list:
-    if num % number == 0:
-        divisors.append(number)
-print(divisors)
