1ff802ba79e722f628bc941d88ab21e6a0cd6ee9
uberdiz <sebastian.r.langdon@gmail.com>
2024-10-10 14:26:26 -0400

    Update SMTv2.py

--- SMTv2.py
+++ SMTv2.py
@@ -64,11 +64,10 @@ def scrape(stock_list, interested):
     for each_stock in stock_list:
         technicals = scrape_yahoo(driver, each_stock)
         stock_data = {"Stock": each_stock.upper()}  # Start with the stock name
-        
-        # Check if any of the values is '--' or 'Not found'
-        #if any(technicals.get(ind.strip(), None) in ('Not found', '--') for ind in interested):
-            #print(f"Skipping {each_stock} due to missing values.")
-            #continue  # Skip to the next stock if any value is invalid
+        # Check if any of the values is '--' or 'Not found' FIXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxx
+        for value in technicals:
+            if value == "--":
+                print()
 
         for ind in interested:
             ind_stripped = ind.strip()  # Strip whitespace from the interested key
@@ -85,7 +84,7 @@ def scrape(stock_list, interested):
     # Convert list of dictionaries into a DataFrame and save to CSV
     df = pd.DataFrame(all_data)
     df.to_csv('!StockHelper_List.csv', index=False)  # Save to CSV
-stock_list = ['CHSN', 'SOFI', 'ALTM', 'SHOP', 'AMD', 'TMUS', 'RIVN', 'SHOP.TO', 'RDDT', 'BENF', 'AXP', 'TIGR', 'FORD', 'ASTS', 'CDE', 'EH', 'OKLO', 'UBI.PA', 'TEAM', 'DECK', 'MSTR', 'GEV', 'LAC', 'BRK-B', 'DIDIY', 'NVDA', 'NIO', 'INND', 'TSLA', 'SWN', 'PLTR', 'EVGO', 'AAL', 'F', 'JBLU', 'ABEV', 'INTC', 'MARA', 'SMCI', 'AMZN', 'JD', 'CCL', 'AAPL', 'IQ', 'LCID', 'BAC', 'WXXWY', 'B', 'NFE', 'ENVX', 'CLSK', 'MMYT', 'WB', 'ASPN', 'VFC', 'ANF', 'NMRA', 'MNSO', 'CFLT', 'DOCN', 'ALB', 'TEM', 'ALAB', 'IOVA', 'ZIM', 'NVAX', 'FND', 'IRDM', 'AMKBY', 'AXSM', 'AZEK', 'NMRK', 'CWK', 'APGE', 'KIROY', 'EXR', 'UWMC', 'NSA', 'SMG', 'TTC', 'BEKE', 'BABA', 'T', 'PFE', 'PWSC', 'BBD', 'RLX', 'NU', 'RIG', 'MU', 'PDD', 'VALE', 'YMM', 'GRAB', 'RIOT', 'GOLD', 'WBD', 'JOBY', 'ITUB', 'XPEV', 'XOM', 'WBA', 'AGNC', 'FUTU', 'SNAP', 'META', 'BILI', 'AVGO', 'CVS', 'GOOGL', 'CLOV', 'KO', 'OXY', 'KGC', 'VST', 'NCLH', 'WFC', 'MSFT', 'VZ', 'WMT', 'PCG', 'FCX', 'GOOG', 'CVE', 'STLA', 'BTE', 'BTG', 'LI', 'HBAN', 'UBER', 'RKLB', 'AES', 'U', 'ET', 'LYG', 'NKE', 'BMY', 'JPM', 'UAL', 'EQT', 'NOK', 'PR', 'HL', 'DJT', 'MRK', 'BP', 'SLB', 'ERIC', 'TGT', 'O', 'MO', '^GSPC', 'CUK']
+stock_list = ['CHSN', 'SOFI', 'ALTM', 'SHOP', 'AMD', 'TMUS', 'RIVN', 'SHOP.TO', 'RDDT', 'BENF', 'AXP', 'TIGR', 'FORD', 'ASTS', 'CDE', 'EH', 'OKLO', 'UBI.PA', 'TEAM', 'DECK', 'MSTR', 'GEV', 'LAC', 'BRK-B', 'DIDIY', 'NVDA', 'NIO', 'INND', 'TSLA', 'PLTR', 'EVGO', 'AAL', 'F', 'JBLU', 'ABEV', 'INTC', 'MARA', 'SMCI', 'AMZN', 'JD', 'CCL', 'AAPL', 'IQ', 'LCID', 'BAC', 'WXXWY', 'B', 'NFE', 'ENVX', 'CLSK', 'MMYT', 'WB', 'ASPN', 'VFC', 'ANF', 'NMRA', 'MNSO', 'CFLT', 'DOCN', 'ALB', 'TEM', 'ALAB', 'IOVA', 'ZIM', 'NVAX', 'FND', 'IRDM', 'AMKBY', 'AXSM', 'AZEK', 'NMRK', 'CWK', 'APGE', 'KIROY', 'EXR', 'UWMC', 'NSA', 'SMG', 'TTC', 'BEKE', 'BABA', 'T', 'PFE', 'BBD', 'RLX', 'NU', 'RIG', 'MU', 'PDD', 'VALE', 'YMM', 'GRAB', 'RIOT', 'GOLD', 'WBD', 'JOBY', 'ITUB', 'XPEV', 'XOM', 'WBA', 'AGNC', 'FUTU', 'SNAP', 'META', 'BILI', 'AVGO', 'CVS', 'GOOGL', 'CLOV', 'KO', 'OXY', 'KGC', 'VST', 'NCLH', 'WFC', 'MSFT', 'VZ', 'WMT', 'PCG', 'FCX', 'GOOG', 'CVE', 'STLA', 'BTE', 'BTG', 'LI', 'HBAN', 'UBER', 'RKLB', 'AES', 'U', 'ET', 'LYG', 'NKE', 'BMY', 'JPM', 'UAL', 'EQT', 'NOK', 'PR', 'HL', 'DJT', 'MRK', 'BP', 'SLB', 'ERIC', 'TGT', 'O', 'MO', 'CUK']
 interested = ['Market Cap', 'Price/Sales', 'Price/Book', 'Return on Equity  (ttm)', 'Revenue  (ttm)', 'Quarterly Earnings Growth  (yoy)', 'Operating Cash Flow  (ttm)', 'Total Cash  (mrq)', 'Total Debt/Equity  (mrq)', 'Current Ratio  (mrq)', '52 Week Range 3', 'Avg Vol (3 month) 3', 'Avg Vol (10 day) 3', '% Held by Insiders 1', 'Diluted EPS  (ttm)', 'Levered Free Cash Flow  (ttm)', 'Forward Annual Dividend Yield 4', 'Trailing Annual Dividend Yield 3', '5 Year Average Dividend Yield 4', 'Price', 'Beta (5Y Monthly)']
 def main(stock_list, interested):
     scrape(stock_list, interested)
